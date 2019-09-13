2a0b28f69c95d4f7bb3fb6bdc7c62857468808dd
zz00024 <zz00024@gmail.com>
2019-09-12 20:54:10 -0700

    start over the findNqueenSolution

--- src/solvers.js
+++ src/solvers.js
@@ -94,31 +94,11 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   //repeat whave we have done for findNRooksSolution
-  //add one more search condition which is checking major diagonal&minor diagonal conflict
-  var solution = new Board({n: n}); //fixme
-  var board = solution.rows();
-  // create a board
-  var col = n;
-  var row = n;
-  // create two variables to represent the row and column
-  // first for loop is looping through all the rows,
-  for (var i = 0; i < row; i++) {
-    for (var j = 0; j < col; j++) {
-      board[i][j] = 1;
-      //add another checking condition here for diagonal conflict
-      if (solution.hasRowConflictAt(i) || solution.hasColConflictAt(j) || solution.hasMajorDiagonalConflictAt(i) || solution.hasMinorDiagonalConflictAt(i)) {
-        board[i][j] = 0;
-      }
-      console.log(board);
-    }
-  }
-  // nested for loop is going through each column in that row
-  // inside the for loop, if statement that checks if there is conflict in the row or column
-  //  change the value from 0 to 1
-  // set var solution to the board we created
+  //add one more search condition which is checking diagonal test
+  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(board));
-  return board;
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
